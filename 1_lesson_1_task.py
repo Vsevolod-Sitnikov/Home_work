7316571df005d41bffbfc7e6a6ac2ddd19c00590
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-27 19:35:38 +0400

    Create 1_lesson_1_task.py

---
+++ 1_lesson_1_task.py
@@ -0,0 +1,33 @@
+var_1 = 'разработка'
+
+var_2 = 'сокет'
+
+var_3 = 'декоратор'
+
+print(var_1)
+print(type(var_1))
+
+print(var_2)
+print(type(var_2))
+
+print(var_3)
+print(type(var_3))
+
+print('----------------------------------------------------')
+#с помощью онлайн-конвертера преобразовал строковые представление в формат Unicode,
+#результат записал в переменные
+
+var_1_uni = '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430'
+
+var_2_uni = '\u0441\u043e\u043a\u0435\u0442'
+
+var_3_uni = '\u0434\u0435\u043a\u043e\u0440\u0430\u0442\u043e\u0440'
+
+print(var_1_uni)
+print(type(var_1_uni))
+
+print(var_2_uni)
+print(type(var_2_uni))
+
+print(var_3_uni)
+print(type(var_3_uni))
