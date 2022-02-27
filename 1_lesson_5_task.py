e2a2b4d86ac34f2e275636e94d23833418fe0e68
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-27 20:32:49 +0400

    Update 1_lesson_5_task.py

--- 1_lesson_5_task.py
+++ 1_lesson_5_task.py
@@ -2,9 +2,14 @@ import chardet
 import subprocess
 import platform
 
+portals = ['yandex.ru', 'youtube.com']
+
 param = '-n' if platform.system().lower() == 'windows' else '-c'
-args = ['ping', param, '2', 'yandex.ru']
-process = subprocess.Popen(args, stdout=subprocess.PIPE)
-for line in process.stdout:
-    result = chardet.detect(line)
-    print(line.decode(result['encoding']))
+for portal in portals:
+    args = ['ping', param, '2', portal]
+    process = subprocess.Popen(args, stdout=subprocess.PIPE)
+    for line in process.stdout:
+        result = chardet.detect(line)
+        print('result = ', result)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
